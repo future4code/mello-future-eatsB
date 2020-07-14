e6b461793faf7a401e3d9f43a65b33bf7a63a7c0
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-14 16:14:16 -0400

    Feature: add Roboto Font

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
     -moz-box-sizing: border-box;
     -webkit-box-sizing: border-box;
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
   }
 `;
 
