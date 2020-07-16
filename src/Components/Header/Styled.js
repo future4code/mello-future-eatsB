08557e6ea2496d6075c6bb046849fa4c5f779db4
maanoeln <mneto_004@hotmail.com>
2020-07-16 17:00:37 -0300

    header pronto

--- src/Components/Header/Styled.js
+++ src/Components/Header/Styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
+
 export const Bar = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
   height: 64px;
@@ -12,3 +13,16 @@ export const Bar = styled.div`
   background-color: #ffffff;
   margin-bottom: 16px;
 `;
+
+export const BarContent = styled.div`
+  flex: 1;
+  display: flex;
+  padding: 16px;
+`
+
+export const Title = styled.div`
+  flex: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
