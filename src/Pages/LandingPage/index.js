62daef0d216ab09ac36398538a368bb2535b578c
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-07-18 11:52:40 -0400

    ( landing ) : landing page redirect to app

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import Logo from '../../Assets/img/Logo.png';
-import { Container } from './styled';
+import React, { useEffect } from "react";
+import Logo from "../../Assets/img/Logo.png";
+import { Container } from "./styled";
+import { useHistory } from "react-router";
 
 const LandingPage = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push("/login");
+    }, 3000);
+  }, [history]);
+
   return (
     <Container>
       <img src={Logo} alt="FutureEats Logo" />
--- src/Pages/Routes/index.js
+++ src/Pages/Routes/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Switch, BrowserRouter } from "react-router-dom";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 import LoginPage from "../LoginPage";
 import SignUpPage from "../SignUpPage";
 import AdressPage from "../AdressPage";
 import FeedPage from "../FeedPage";
+
 import RestaurantsPage from "../RestaurantsPage";
 import ConfirmationPage from "../ConfirmationPage";
 import ProfilePage from "../ProfilePage";
@@ -21,9 +22,9 @@ const Routes = () => {
     <>
       <BrowserRouter>
         <Switch>
-          <LoginRoute exact path="/">
+          <Route exact path="/">
             <LandingPage />
-          </LoginRoute>
+          </Route>
           <LoginRoute exact path="/login">
             <LoginPage />
           </LoginRoute>
